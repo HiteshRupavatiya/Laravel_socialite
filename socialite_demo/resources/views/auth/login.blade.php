35b16052435f1fa89639f3238ee9e261d851ef10
hitesh <hiteshr@zignuts.com>
2023-03-13 19:34:33 +0530

    anchor links added in blade for calling login routes

--- socialite_demo/resources/views/auth/login.blade.php
+++ socialite_demo/resources/views/auth/login.blade.php
@@ -11,11 +11,21 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="form-group row mb-3">
+                            <div class="col-md-7 offset-md-3">
+                                <a href="{{ route('login.github') }}" class="btn btn-dark btn-block">Login with Github</a>
+                                <a href="{{ route('login.google') }}" class="btn btn-danger btn-block">Login with Google</a>
+                                {{-- <a href="{{ route('login.facebook') }}" class="btn btn-primary btn-block">Login with Facebook</a> --}}
+                            </div>
+                        </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <p style="text-align: center">OR</p>
+
+                        <div class="row mb-3">
+                            {{-- <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label> --}}
+
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail Address">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +36,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            {{-- <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label> --}}
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -40,7 +50,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -52,7 +62,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-3">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
